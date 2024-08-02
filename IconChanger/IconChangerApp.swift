65153c1dd0472f34d0bf6686d0a48bf5e03805e5
ngoclx <2ndev.vn@gmail.com>
2024-08-02 15:56:21 +0700

    Initial Commit

---
+++ IconChanger/IconChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconChangerApp.swift
+//  IconChanger
+//
+//  Created by Lee Lucci on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct IconChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
